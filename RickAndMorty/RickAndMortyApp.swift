7bbbcc3d102ff573dd890b37708ea9c1fa1ae4c7
Christopher Alford <chrisvalford@me.com>
2023-06-07 19:10:49 +0200

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,20 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Christopher Alford on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
