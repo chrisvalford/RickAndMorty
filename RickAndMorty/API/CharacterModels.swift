034c68e9582b3f8deeacc2ee1abfd18b2c166f1c
Christopher Alford <chrisvalford@me.com>
2023-06-12 13:55:01 +0200

    Missed from last xcode commit

--- RickAndMorty/API/CharacterModels.swift
+++ RickAndMorty/API/CharacterModels.swift
@@ -7,39 +7,6 @@
 
 import Foundation
 
-public enum CharacterState: String, Codable {
-    case unknown
-    case alive
-    case dead
-
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        let rawString = try container.decode(String.self)
-        if let state = CharacterState(rawValue: rawString.lowercased()) {
-            self = state
-        } else {
-            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Cannot initialize CharacterState from invalid String value \(rawString)")
-        }
-    }
-}
-
-public enum CharacterGender: String, Codable {
-    case unknown
-    case female
-    case male
-    case genderless
-
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        let rawString = try container.decode(String.self)
-        if let gender = CharacterGender(rawValue: rawString.lowercased()) {
-            self = gender
-        } else {
-            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Cannot initialize CharacterGender from invalid String value \(rawString)")
-        }
-    }
-}
-
 public struct CharacterResults: Decodable {
     let info: Info
     let results: [SeriesCharacter]
